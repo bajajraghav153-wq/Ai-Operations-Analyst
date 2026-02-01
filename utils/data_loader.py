5e6438f723e7b42e45a609952020b2623f32044a
bajajraghav153-wq <bajajraghav153@gmail.com>
2026-02-01 07:27:20 +0530

    Create data_loader.py

---
+++ utils/data_loader.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def load_csv(file):
+    try:
+        df = pd.read_csv(file)
+        df.columns = [c.strip().lower().replace(" ", "_") for c in df.columns]
+        return df
+    except Exception as e:
+        return None
